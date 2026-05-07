962ecfd8ebd18b4c2b15674709afd448d0e0256b
Sean Dursi <seanrobertdursi@gmail.com>
2026-05-07 18:40:06 -0400

    Task 3.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    static void main(String args) {
 
         Integer i = 1;
         while (i < 100) {
@@ -11,7 +11,7 @@ class FizzBuzz {
         }
     }
 
-    private static Integer doFizzBuzz(Integer i) {
+    static Integer doFizzBuzz(Integer i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main() {
+        FizzBuzz.doFizzBuzz(5);
+        Integer a = 5;
+        String s = new String(a.toString());
+        FizzBuzz.main(s);
+    }
+}
